ee434231c7c2829aaa24d210c052fbdfb3ef9f21
Htetyinsoe <htetyysoe@gmail.com>
2025-08-27 14:38:11 +0700

    added basic java script and type writer motion

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-//alert("hello");
+function generateRecipe(event) {
+    event.preventDefault();
+    let generatedRecipeElement = document.querySelector("#recipe-output");
+    generatedRecipeElement.innerHTML = "Here is your generated recipe: ... (This is a placeholder)";
+    
+    new Typewriter('#recipe-output', {
+  strings: "Here is your generated recipe: ... (This is a placeholder)",
+  autoStart: true,
+  delay: 1,
+  cursor: " ",
+});
+}
+
+let recipeFormElement = document.querySelector("#recipe-form");
+recipeFormElement.addEventListener("submit", generateRecipe);
